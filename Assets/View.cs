0710b0807422a14a36861c747b0997c1c23bb70e
NTBlion <103928473+NTBlion@users.noreply.github.com>
2024-07-24 16:17:55 +0300

    MVP

--- Assets/View.cs
+++ Assets/View.cs
@@ -2,18 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class View : MonoBehaviour
 {
     [SerializeField] private TMP_Text _intValue;
     [SerializeField] private TMP_Text _stringValue;
+    [SerializeField] private Presenter _presenter;
+    
+    [SerializeField] private Button _plusBotton;
+    [SerializeField] private Button _minusBotton;
+    [SerializeField] private TMP_InputField _inputField;
 
-    public void ChangeIntValue(int value)
+    private void OnEnable()
+    {
+        _plusBotton.onClick.AddListener(OnClickPlusButton);
+        _minusBotton.onClick.AddListener(OnClickMinusButton);
+        _inputField.onEndEdit.AddListener(OnEdit);
+    }
+
+    private void OnEdit(string text)
+    {
+        _presenter.OnEdit(text);
+        ChangeStringValue(text);
+    }
+
+    private void OnClickMinusButton()
+    {
+        _presenter.OnClickMinusButton();
+        ChangeIntValue(_presenter.Model.Value);
+    }
+
+    private void OnClickPlusButton()
+    {
+        _presenter.OnClickPlusButton();
+        ChangeIntValue(_presenter.Model.Value);
+    }
+
+    private void OnDisable()
+    {
+        _plusBotton.onClick.RemoveListener(OnClickPlusButton);
+        _minusBotton.onClick.RemoveListener(OnClickMinusButton);
+        _inputField.onEndEdit.RemoveListener(OnEdit);
+    }
+    
+    private void ChangeIntValue(int value)
     {
         _intValue.text = value.ToString();
     }
 
-    public void ChangeStringValue(string value)
+    private void ChangeStringValue(string value)
     {
         _stringValue.text = value;
     }
